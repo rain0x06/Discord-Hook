e19f68c0446cfeab31f7478f083485eacf753756
rain- <87215040+rain1337lol@users.noreply.github.com>
2021-09-12 14:31:25 -0700

    Add files via upload

---
+++ dllmain.cpp
@@ -0,0 +1,198 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <Windows.h>
+#include <iostream>
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_dx11.h"
+#include "imgui/imgui_internal.h"
+
+#include "scanner.h"
+#include <d3d11.h>
+
+ID3D11RenderTargetView* rendertarget;
+ID3D11DeviceContext* context;
+ID3D11Device* device;
+HRESULT(*present_original)(IDXGISwapChain* swapchain, UINT sync, UINT flags) = nullptr;
+int X, Y;
+HWND hwnd;
+
+namespace DiscordHelper
+{
+	std::vector<uintptr_t> pCreatedHooksArray;
+	bool CreateHook(uintptr_t pOriginal, uintptr_t pHookedFunction, uintptr_t pOriginalCall)
+	{
+		static uintptr_t addrCreateHook = NULL;
+
+		if (!addrCreateHook)
+			addrCreateHook = sigscan("DiscordHook64.dll", ("41 57 41 56 56 57 55 53 48 83 EC 68 4D 89 C6 49 89 D7"));
+
+		if (!addrCreateHook)
+			return false;
+
+		using CreateHook_t = uint64_t(__fastcall*)(LPVOID, LPVOID, LPVOID*);
+		auto fnCreateHook = (CreateHook_t)addrCreateHook;
+
+		return SpoofCall(fnCreateHook, (void*)pOriginal, (void*)pHookedFunction, (void**)pOriginalCall) == 0 ? true : false;
+	}
+
+	bool EnableHookQue()
+	{
+		static uintptr_t addrEnableHookQueu = NULL;
+
+		if (!addrEnableHookQueu)
+			addrEnableHookQueu = sigscan("DiscordHook64.dll", ("41 57 41 56 41 55 41 54 56 57 55 53 48 83 EC 38 48 ? ? ? ? ? ? 48 31 E0 48 89 44 24 30 BE 01 00 00 00 31 C0 F0 ? ? ? ? ? ? ? 74 2B"));
+
+		if (!addrEnableHookQueu)
+			return false;
+
+		using EnableHookQueu_t = uint64_t(__stdcall*)(VOID);
+		auto fnEnableHookQueu = (EnableHookQueu_t)addrEnableHookQueu;
+
+		return SpoofCall(fnEnableHookQueu) == 0 ? true : false;
+	}
+
+	short GetAsyncKeyState(int key)
+	{
+		static uintptr_t GetAsyncKeyState_addr;
+		if (!GetAsyncKeyState_addr)
+			GetAsyncKeyState_addr = sigscan("DiscordHook64.dll", ("48 FF ? ? ? ? ? CC CC CC CC CC CC CC CC CC 48 FF ? ? ? ? ? CC CC CC CC CC CC CC CC CC 48 83 EC 28 48 ? ? ? ? ? ? 48 85 C9"));
+		if (!GetAsyncKeyState_addr)
+			return false;
+
+		auto queuehook = ((short(__fastcall*)(int))(GetAsyncKeyState_addr));
+		return SpoofCall(queuehook, key);
+	}
+
+	//this
+	bool EnableHook(uintptr_t pTarget, bool bIsEnabled)
+	{
+		static uintptr_t addrEnableHook = NULL;
+
+		if (!addrEnableHook)
+			addrEnableHook = sigscan("DiscordHook64.dll", ("41 56 56 57 53 48 83 EC 28 49 89 CE BF 01 00 00 00 31 C0 F0 ? ? ? ? ? ? ? 74"));
+
+		if (!addrEnableHook)
+			return false;
+
+		using EnableHook_t = uint64_t(__fastcall*)(LPVOID, bool);
+		auto fnEnableHook = (EnableHook_t)addrEnableHook;
+
+		return SpoofCall(fnEnableHook, (void*)pTarget, bIsEnabled) == 0 ? true : false;
+	}
+
+	bool InsertHook(uintptr_t pOriginal, uintptr_t pHookedFunction, uintptr_t pOriginalCall)
+	{
+		bool bAlreadyCreated = false;
+		for (auto _Hook : pCreatedHooksArray)
+		{
+			if (_Hook == pOriginal)
+			{
+				bAlreadyCreated = true;
+				break;
+			}
+		}
+
+		if (!bAlreadyCreated)
+			bAlreadyCreated = CreateHook(pOriginal, pHookedFunction, pOriginalCall);
+
+		if (bAlreadyCreated)
+			if (EnableHook(pOriginal, true))
+				if (EnableHookQue())
+					return true;
+
+		return false;
+	}
+}
+
+HRESULT present_hooked(IDXGISwapChain* swapchain, UINT sync, UINT flags)
+{
+    if (!device)
+    {
+        ID3D11Texture2D* renderTarget = 0; 
+        ID3D11Texture2D* backBuffer = 0;
+        D3D11_TEXTURE2D_DESC backBufferDesc = { 0 };
+
+        swapchain->GetDevice(__uuidof(device), (PVOID*)&device);
+        device->GetImmediateContext(&context);
+
+        swapchain->GetBuffer(0, __uuidof(renderTarget), (PVOID*)&renderTarget);
+        device->CreateRenderTargetView(renderTarget, nullptr, &rendertarget);
+        renderTarget->Release();
+
+        swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (PVOID*)&backBuffer);
+        backBuffer->GetDesc(&backBufferDesc);
+
+        X = backBufferDesc.Width;
+        Y = backBufferDesc.Height;
+
+        backBuffer->Release();
+
+        if (!hwnd)
+        {
+            //Change window name and class for the specific game, to check use spy++ its in the tool tab of visual studio :D
+            //For loading a non steam game for example rogue company press add a game -> add a non-steam game
+            hwnd = FindWindowW(L"UnrealWindow", L"Fortnite  ");
+            //if not found try to use foreground window?
+            if (!hwnd)
+                hwnd = GetForegroundWindow();
+        }
+
+        ImGui_ImplDX11_Init(hwnd, device, context);
+        ImGui_ImplDX11_CreateDeviceObjects();
+    }
+    context->OMSetRenderTargets(1, &rendertarget, nullptr);
+
+    ImGui_ImplDX11_NewFrame();
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+    ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0, 0, 0, 0));
+    ImGui::Begin("##scene", nullptr, ImGuiWindowFlags_NoInputs | ImGuiWindowFlags_NoTitleBar);
+    ImGuiIO& IO = ImGui::GetIO();
+    ImGui::SetWindowPos(ImVec2(0, 0), ImGuiCond_Always);
+    ImGui::SetWindowSize(ImVec2(IO.DisplaySize.x, IO.DisplaySize.y), ImGuiCond_Always);
+    ImGuiWindow& window = *ImGui::GetCurrentWindow();
+
+
+
+    window.DrawList->AddText(ImVec2(50, 100), ImGui::GetColorU32(ImVec4(1.f, 0.f, 0.f, 1.f)), "working");
+
+
+
+    window.DrawList->PushClipRectFullScreen();
+    ImGui::PopStyleColor();
+    ImGui::PopStyleVar(2);
+    ImGui::Render();
+
+    return present_original(swapchain, sync, flags);
+}
+
+void init()
+{
+    if (!GetModuleHandleA("DiscordHook64.dll"))
+    {
+        MessageBoxA(0, "DiscordHook64.dll Not Loaded!", "Fatal Error", MB_ICONERROR);
+        exit(0);
+    }
+
+    //create console
+    AllocConsole();
+    freopen("CONIN$", "r", stdin);
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONOUT$", "w", stderr);
+
+    uintptr_t presentscene = sigscan("DiscordHook64.dll", ("56 57 53 48 83 EC 30 44 89 C6"));
+
+    DiscordHelper::InsertHook(presentscene, (uintptr_t)present_hooked, (uintptr_t)&present_hooked);
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+        init();
+
+    return TRUE;
+}
+
